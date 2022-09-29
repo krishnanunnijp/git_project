2b765b4ef376088b8df2ce984382becc2a26c900
krishnanunnijp <krishnanunni.j@nestgroup.net>
2022-09-29 13:25:01 +0530

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world! this is a git project");
+        person person1 = new person();
+
 
     }
 
---
+++ src/person.java
@@ -0,0 +1,5 @@
+public class person {
+    private String stri="yeah its working";
+    public void pri(){
+    System.out.println(this.stri);}
+}
