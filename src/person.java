8f1bf6a5d612855e404466b898d238b97c4a57f9
krishnanunnijp <krishnanunni.j@nestgroup.net>
2022-09-29 14:28:05 +0530

    new branch commit

--- src/person.java
+++ src/person.java
@@ -1,5 +1,6 @@
 public class person {
-    private String stri="yeah its working";
+    private String stri="yeah its working in new_change branch";
     public void pri(){
     System.out.println(this.stri);}
+
 }
